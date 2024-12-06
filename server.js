beb5ba75eea90f2a347eda2e7f49da5d4bba5f7a
Filldreamwork <workfill1997@gmail.com>
2024-12-06 13:40:36 +0300

    Delete server.js

--- server.js
+++
@@ -1,7 +0,0 @@
-const express = require('express');
-const app = express();
-
-const PORT = process.env.PORT || 3000; // Используем порт из среды, если он есть
-app.listen(PORT, '0.0.0.0', () => {
-    console.log(`Сервер запущен на порту ${PORT}`);
-});
